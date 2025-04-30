170eb3905122c265a3596e019a0ab824b7d98448
Y98 <81143588+3wdy98@users.noreply.github.com>
2025-04-30 19:20:15 +0300

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ author 'Y98'
 description 'Arrow Mini Game UI'
 version '1.0.0'
 
-ui_page 'html/index.html'
+ui_page 'index.html'
 
 client_scripts {
     'client.lua'
@@ -16,7 +16,7 @@ server_scripts {
 }
 
 files {
-    'html/index.html',
+    'index.html',
 }
 
 exports {
